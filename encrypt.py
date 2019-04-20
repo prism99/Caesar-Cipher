351f50cc91bf685db35a182bb3572de83be5303f
Prism <45443411+prism99@users.noreply.github.com>
2019-04-20 00:01:19 -0400

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -4,10 +4,10 @@
 Caesar Cipher's by Kyle Bilak kbilak@email.uscb.edu
 April 18 2019 
 """
-unencrypted_text = raw_input("Enter your message to encrypt: ")
+unencrypted_message = raw_input("Enter your message to encrypt: ")
  
 encrypted_message = ""
-for i in unencrypted_text:
+for i in unencrypted_message:
     x = ord(i) # take letters unicode value 
     x = x + 1  # add one to that value
     x2 = chr(x) # convert int number back to letter
